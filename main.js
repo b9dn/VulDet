9ce89a8ce4a6fe4b3fb9754d24bda230aeda418e
Your Name <you@example.com>
2026-02-10 19:10:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,149 @@
+import dotenv from "dotenv";
+import sqlite from "sqlite3";
+import fs from "fs";
+import { GoogleGenAI } from "@google/genai";
+
+dotenv.config();
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI });
+const db = new sqlite.Database("./data.sqlite");
+
+// const name = "tngtech/deepseek-r1t2-chimera:free";
+const name = "gemini-2.5-flash";
+const isGemini = true;
+const requestLimit = 9;
+const sleepTime = 10;
+
+const sendMessageOR = async (data, model, key = process.env.OPENROUTER) => {
+  const messages = [
+    {
+      role: "user",
+      content: `Analyze the following c or cpp code for vulnerabilities.
+
+Rules:
+- Return exactly one word: "Safe" or "Vulnerable".
+- Do not explain your answers.
+- Do not rewrite the code.
+- Do not add any extra text.
+
+Code:
+\`\`\`
+${data.code}
+\`\`\``,
+    },
+  ];
+
+  const response = await fetch(
+    "https://openrouter.ai/api/v1/chat/completions",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${key}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: model,
+        messages: messages,
+        reasoning: { enabled: true },
+      }),
+    },
+  );
+
+  const result = await response.json();
+
+  console.log(result);
+  console.log(result.choices[0].message.content);
+
+  return {
+    id: data.id,
+    expected: data.isVulnerable ? "Vulnerable" : "Safe",
+    received: result.choices[0].message.content.replace(/\n/g, ""),
+    numTokensInput: result.usage.prompt_tokens,
+    numTokensOutput: result.usage.completion_tokens,
+  };
+};
+
+const sendMessageGemini = async (data, model) => {
+  const message = `Analyze the following functions: ${data.names} written in c programming language for vulnerabilities.
+
+Rules:
+- Return exactly one word: "Safe" or "Vulnerable".
+- Do not explain your answers.
+- Do not rewrite the code.
+- Do not add any extra text.
+Code:
+      \`\`\`
+      ${data.codeContext}
+      \`\`\``;
+
+  const response = await ai.models.generateContent({
+    model: model,
+    contents: message,
+  });
+
+  return {
+    id: data.id,
+    expected: "Safe",
+    received: response.text.replace(/\n/g, ""),
+    numTokensInput: response.usageMetadata.promptTokenCount,
+    numTokensOutput: response.usageMetadata.candidatesTokenCount,
+    numTokensThought: response.usageMetadata.thoughtsTokenCount,
+  };
+};
+
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const path = `./results/${name.replace(/\//g, "")}-CONTEXT.json`;
+let prevResults;
+
+if (fs.existsSync(path)) {
+  const data = fs.readFileSync(path);
+  prevResults = JSON.parse(data);
+} else {
+  prevResults = [];
+}
+
+const checkedIds = prevResults.map((val) => {
+  return val.id;
+});
+
+const whereClause = checkedIds.length
+  ? `WHERE id NOT IN (${checkedIds.join(",")})`
+  : "";
+
+const promises = [];
+
+db.all(
+  `SELECT * FROM data ${whereClause} LIMIT ${requestLimit}`,
+  async (err, rows) => {
+    if (err) return console.error(err.message);
+    for (const data of rows) {
+      if (isGemini) {
+        promises.push(
+          sendMessageGemini(data, name).catch((err) => {
+            console.error("Error", err.message);
+            return null;
+          }),
+        );
+      } else {
+        promises.push(
+          sendMessageOR(data, name).catch((err) => {
+            console.error("Error", err.message);
+            return null;
+          }),
+        );
+      }
+
+      await sleep(sleepTime * 1000);
+    }
+
+    Promise.all(promises).then((currResults) => {
+      const combined = [
+        ...prevResults,
+        ...currResults.filter((item) => item !== null),
+      ];
+      fs.writeFileSync(path, JSON.stringify(combined));
+    });
+  },
+);
---
+++ manual.js
@@ -0,0 +1,106 @@
+import sqlite from "sqlite3";
+import fs from "fs";
+import readline from "readline";
+import clipboard from "clipboardy";
+
+const db = new sqlite.Database("data.sqlite");
+
+const name = "Sonnet 4.5";
+const samplesInOneRequestNum = 5;
+const path = `./results/${name.replace(/\//g, "")}-MANUAL.json`;
+let prevResults;
+
+if (fs.existsSync(path)) {
+  const data = fs.readFileSync(path);
+  prevResults = JSON.parse(data);
+} else {
+  prevResults = [];
+}
+
+const checkedIds = prevResults.map((val) => {
+  return val.id;
+});
+
+const whereClause = checkedIds.length
+  ? `WHERE id NOT IN (${checkedIds.join(",")})`
+  : "";
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function askQuestion(query) {
+  return new Promise((resolve) => rl.question(query, resolve));
+}
+
+const initMessage = `Your job is to review code for vulnerabilities and security risks.
+
+From now on for any given question you will be given code samples for security vulnerabilities and your job is to answer based on given rules and format below.
+
+Rules:
+- Evaluate each code sample independently.
+- For each sample, return exactly one word: "Safe" or "Vulnerable".
+- Do not explain your answers.
+- Do not rewrite the code or provide explanation.
+- Do not add any extra text.
+- Do not ask questions
+
+Output format (in one line) chose either Safe or Vulnerable and devide answers with ; example:
+Sample 1: Safe | Vulnerable;Sample 2: Safe | Vulnerable
+
+`;
+
+let counter = 0;
+const currResults = [];
+
+db.all(`SELECT id, code FROM data ${whereClause}`, async (err, rows) => {
+  while (true) {
+    try {
+      const chosenRows = rows.slice(
+        counter * samplesInOneRequestNum,
+        (counter + 1) * samplesInOneRequestNum,
+      );
+      const text = chosenRows
+        .map(
+          (el, index) => `
+Sample ${index + 1}:
+\`\`\`
+${el.code}
+\`\`\`
+`,
+        )
+        .join();
+
+      clipboard.writeSync(initMessage + text);
+
+      const answer = await askQuestion("Answer: ");
+
+      if (answer === "exit") {
+        break;
+      }
+      console.log(answer);
+      const formattedResponse = answer.split(";");
+      if (formattedResponse.length !== chosenRows.length) {
+        console.error("Invalid response data length");
+      }
+
+      for (let i = 0; i < chosenRows.length; i++) {
+        currResults.push({
+          id: chosenRows[i].id,
+          expected: "Safe",
+          received: formattedResponse[i].split(":")[1].trim(),
+        });
+      }
+
+      counter++;
+    } catch (e) {
+      console.log(e);
+      break;
+    }
+  }
+
+  rl.close();
+  const combined = [...prevResults, ...currResults];
+  fs.writeFileSync(path, JSON.stringify(combined));
+});
